cff5b63038b8d7ead72ea813ce62cc471025445d
Steffen Wold <woldsteffen@gmail.com>
2022-09-19 09:14:01 +0200

    add docs

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -1,17 +1,30 @@
 <h1>Axess Documentation</h1>
 
-<div class="wide-extra-small display-flex">
-  <div>
-    <h3>Get Started</h3>
-    <p>Instructions on how to get started designing with Axess.</p>  
-  </div>
-  <div>
-    <svg class="narrow-medium short-medium" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="m15.038 6.343-1.411 1.418 3.27 3.255-13.605.013.002 2 13.568-.013-3.215 3.23 1.417 1.41 5.644-5.67-5.67-5.643Z" fill="currentColor"/>
-    </svg>
-  </div>
+<div class="display-grid auto-fit-medium gap-medium">
+  <a class="hover-reset-underline" href="/docs/start">
+    <div class="padding-all-medium display-flex border-all border-dark-lighten hover-background-dark animate">
+      <div>
+        <h3>Get Started</h3>
+        <p>Instructions on how to get started designing with Axess.</p>  
+      </div>
+      <div class="short-small display-flex align-items-center gap-medium cursor-pointer" on:click={() => goto("/docs/tags")}>
+        <svg class="parent-height" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.268 4.21a.75.75 0 0 0-1.04 1.08l8.275 7.96H3.75a.75.75 0 0 0 0 1.5h18.752l-8.273 7.959a.75.75 0 0 0 1.04 1.08l9.428-9.069a1 1 0 0 0 0-1.441l-9.428-9.07Z" fill="currentColor" fill-rule="nonzero"/>
+        </svg>
+      </div>
+    </div>
+  </a>
+  <a class="hover-reset-underline" href="/docs/start">
+    <div class="padding-all-medium display-flex border-all border-dark-lighten hover-background-dark animate">
+      <div>
+        <h3>Get Started</h3>
+        <p>Instructions on how to get started designing with Axess.</p>  
+      </div>
+      <div class="short-small display-flex align-items-center gap-medium cursor-pointer" on:click={() => goto("/docs/tags")}>
+        <svg class="parent-height" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.268 4.21a.75.75 0 0 0-1.04 1.08l8.275 7.96H3.75a.75.75 0 0 0 0 1.5h18.752l-8.273 7.959a.75.75 0 0 0 1.04 1.08l9.428-9.069a1 1 0 0 0 0-1.441l-9.428-9.07Z" fill="currentColor" fill-rule="nonzero"/>
+        </svg>
+      </div>
+    </div>
+  </a>
 </div>
-
-<style>
-
-</style>
--- src/routes/docs/tags/[slug]/+page.svelte
+++ src/routes/docs/tags/[slug]/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-	import { goto } from '$app/navigation'
+  import { goto } from '$app/navigation'
   import { slugify } from '$lib/utils'
 
-	export let data
+  export let data
 
-	let item = data.item
+  let item = data.item
   let itemDesc = `Axess ${item.desc} reference.`
 </script>
 
@@ -18,11 +18,25 @@
 	<meta property="og:image" content="" />
 </svelte:head>
 
-<h1>{item.tag}</h1>
-<p>{itemDesc}</p>
-
-<div class="button cta" on:click={() => goto("/tags")}>
-	<svg class="flip-x" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
-		<path d="M15.268 4.21a.75.75 0 0 0-1.04 1.08l8.275 7.96H3.75a.75.75 0 0 0 0 1.5h18.752l-8.273 7.959a.75.75 0 0 0 1.04 1.08l9.428-9.069a1 1 0 0 0 0-1.441l-9.428-9.07Z" fill="currentColor" fill-rule="nonzero"/>
-	</svg>
+<div class="display-flex align-items-center justify-space-between">
+	<h1>{item.tag}</h1>
+	<div class="short-small display-flex align-items-center gap-medium cursor-pointer" on:click={() => goto("/docs/tags")}>
+		<svg class="parent-height flip-y--180" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+			<path d="M15.268 4.21a.75.75 0 0 0-1.04 1.08l8.275 7.96H3.75a.75.75 0 0 0 0 1.5h18.752l-8.273 7.959a.75.75 0 0 0 1.04 1.08l9.428-9.069a1 1 0 0 0 0-1.441l-9.428-9.07Z" fill="currentColor" fill-rule="nonzero"/>
+		</svg>
+		<span>Return</span>
+	</div>
 </div>
+
+<p class="margin-top-small padding-all-medium background-dark">{itemDesc}</p>
+
+<h2 class="padding-top-medium">Semantic Design</h2>
+<div class="padding-all-medium">
+	This is the default <a href="/docs/tags/a">hyperlink</a>.
+</div>
+<pre class="padding-all-medium background-dark overflow-horizontal-auto"><code>This is a default &#x3C;a href=&#x22;/docs/tags/a&#x22;&#x3E;hyperlink&#x3C;/a&#x3E;.</code></pre>
+<h2 class="padding-top-medium">Example Design</h2>
+<div class="padding-all-medium">
+	This is a styled <a class="padding-all-small background-dark text-light-darken hover-text-light hover-reset-underline animate" href="/docs/tags/a">hyperlink</a>.
+</div>
+<pre class="padding-all-medium background-dark overflow-horizontal-auto"><code>This is a styled &#x3C;a class=&#x22;padding-all-small background-dark text-light-darken hover-text-light hover-reset-underline animate&#x22; href=&#x22;/docs/tags/a&#x22;&#x3E;hyperlink&#x3C;/a&#x3E;.</code></pre>
