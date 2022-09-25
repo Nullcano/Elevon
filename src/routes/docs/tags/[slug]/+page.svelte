51c79cf0b1e62e5744a65c2ba082dcbf4e2b90df
Nullcano <mo@shi.moe>
2022-09-25 16:55:27 +0200

    fixes

--- src/routes/docs/tags/[slug]/+page.svelte
+++ src/routes/docs/tags/[slug]/+page.svelte
@@ -21,26 +21,26 @@
 <div class="padding-vertical-4 display-flex align-items-center justify-space-between">
 	<h1>{item.tag}</h1>
 	<div class="display-flex align-items-center gap-medium cursor-pointer" on:click={() => goto("/docs/tags")}>
-		<svg class="width-7 height-7 flip-y--180" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
+		<svg class="width-200 height-200 flip-y--180" viewBox="0 0 28 28" xmlns="http://www.w3.org/2000/svg">
 			<path d="M15.268 4.21a.75.75 0 0 0-1.04 1.08l8.275 7.96H3.75a.75.75 0 0 0 0 1.5h18.752l-8.273 7.959a.75.75 0 0 0 1.04 1.08l9.428-9.069a1 1 0 0 0 0-1.441l-9.428-9.07Z" fill="currentColor" fill-rule="nonzero"/>
 		</svg>
 		<span>Return to tags</span>
 	</div>
 </div>
 
-<p class="padding-4 background-dark">{itemDesc}</p>
+<p class="padding-4 background-light">{itemDesc}</p>
 
 {#each item.previews as preview}
-	<div class="width-parent margin-top-7 display-flex gap-medium">
+	<div class="width-parent margin-top-7 display-flex gap-100">
 		<div class="width-half">
 			<h2>Semantic Design</h2>
 			<div class="padding-5">
 				{@html preview.semantic}
 			</div>
 			<div>
-				<div class="padding-vertical-2 padding-horizontal-3 text-3 border-all border-regular border-dark">HTML</div>
+				<div class="padding-vertical-2 padding-horizontal-3 text-3 border-all border-regular border-light">HTML</div>
 				<div>
-					<pre class="background-dark overflow-horizontal-auto"><code class="padding-4">{preview.semantic}</code></pre>	
+					<pre class="background-light overflow-horizontal-auto"><code class="padding-4">{preview.semantic}</code></pre>	
 				</div>
 			</div>
 		</div>
@@ -50,9 +50,9 @@
 				{@html preview.example}
 			</div>
 			<div>
-				<div class="padding-vertical-2 padding-horizontal-3 text-3 border-all border-regular border-dark">HTML</div>
+				<div class="padding-vertical-2 padding-horizontal-3 text-3 border-all border-regular border-light">HTML</div>
 				<div>
-					<pre class="background-dark overflow-horizontal-auto"><code class="padding-4">{preview.example}</code></pre>
+					<pre class="background-light overflow-horizontal-auto"><code class="padding-4">{preview.example}</code></pre>
 				</div>
 			</div>
 		</div>
