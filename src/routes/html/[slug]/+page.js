1c2155da733c293c8b1ede2eea32b078f69cc57c
Nullcano <woldsteffen@gmail.com>
2023-02-21 07:17:18 +0100

    depr

--- src/routes/html/[slug]/+page.js
+++
@@ -1,7 +0,0 @@
-import { meta, elements } from '$lib/api/elements'
-import { slugify } from '$lib/utils'
-
-export async function load({ params }) {
-  const { slug } = params
-  const items = elements.filter(i => slugify(i.category) === slug)
-}
